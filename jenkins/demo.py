3975a9cc7eea4d71c9a73468c1d5c400c65fa13d
venkat <venkat44488@gmail.com>
2023-09-07 21:01:31 +0530

    jenkins readme.md

--- jenkins/demo.py
+++ jenkins/demo.py
@@ -1,1 +1,13 @@
 
+# JENKINS DATA as below
+- Manage Jenkins -->Plugins for plugin installaiton.
+- Manage Jenkins --> System
+- Manage Jenkins --> tools
+- Manage Jenkins  --> nodes and clouds
+- Manage Jenkins --> backup manager
+- Manage Jenkins -- > security 
+- Manage Jenkins --> credentials 
+- Manage Jenkins --> system information : will show complete system information like 
+    - jenkins version,java verison, windows os and version, 
+    - workspace home path, java home etc .
+
