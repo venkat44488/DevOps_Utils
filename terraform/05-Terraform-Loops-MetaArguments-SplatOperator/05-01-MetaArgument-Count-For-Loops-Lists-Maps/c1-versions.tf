fcb4c27930b5e6bcc5bad7c7b100b358a2dd9ad8
venkat <venkat44488@gmail.com>
2023-09-07 13:36:23 +0530

    Terraform For Loops, Lists, Maps and Count Meta-Argument

---
+++ terraform/05-Terraform-Loops-MetaArguments-SplatOperator/05-01-MetaArgument-Count-For-Loops-Lists-Maps/c1-versions.tf
@@ -0,0 +1,17 @@
+# Terraform Settings Block
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.21" # Optional but recommended in production
+    }
+  }
+}
+
+# Provider Block
+provider "aws" {
+  profile = "default" # AWS Credentials Profile configured on your local desktop terminal  $HOME/.aws/credentials
+  region  = "us-east-1"
+}
+
+ 
